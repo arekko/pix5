c6c6b64f785400b9f029cafdb93df88fb60bf2d5
Andrei Vasilev <andreidev@arekko.home>
2019-05-31 10:47:46 +0300

    Add unsplash API image types
    Add unsplash API image types

---
+++ src/types/index.ts
@@ -0,0 +1,101 @@
+// Image interfaces
+export interface Image {
+  id: string;
+  created_at: string;
+  updated_at: string;
+  width: number;
+  height: number;
+  color: string;
+  downloads?: number;
+  likes: number;
+  liked_by_user: boolean;
+  description: string;
+  exif?: Exif;
+  location?: Location;
+  tags?: Tag[];
+  current_user_collections: Collection[];
+  urls: Urls;
+  links: ImageLinks;
+  user: User;
+}
+
+export interface Tag {
+  title: string;
+}
+
+export interface Exif {
+  make: string;
+  model: string;
+  exposure_time: string;
+  aperture: string;
+  focal_length: string;
+  iso: number;
+}
+
+export interface Location {
+  city: string;
+  country: string;
+  position: Position;
+}
+
+export interface Position {
+  latitude: number;
+  longitude: number;
+}
+
+export interface Collection {
+  id: number;
+  title: string;
+  published_at: string;
+  updated_at: string;
+  curated: boolean;
+  cover_photo?: string;
+  user: User | null;
+}
+
+export interface User {
+  id: string;
+  username: string;
+  name: string;
+  portfolio_url: string | null;
+  bio: string;
+  location: string;
+  total_likes: number;
+  total_photos: number;
+  total_collections: number;
+  instagram_username: string;
+  twitter_username: string;
+  profile_image: ProfileImage;
+  links: UserLinks;
+}
+
+export interface ProfileImage {
+  small: string;
+  medium: string;
+  large: string;
+}
+
+export interface Urls {
+  raw: string;
+  full: string;
+  regular: string;
+  small: string;
+  thumg: string;
+}
+
+// Links interfaces
+export interface Links {
+  self: string;
+  html: string;
+}
+
+export interface ImageLinks extends Links {
+  download: string;
+  download_location: string;
+}
+
+export interface UserLinks extends Links {
+  photos: string;
+  likes: string;
+  portfolio: string;
+}
